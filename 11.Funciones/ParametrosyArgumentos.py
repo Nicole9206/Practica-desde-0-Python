d1528da9b1488241ee3081465f2ea881b4542366
Nicole9206 <nic.latfig@gmail.com>
2024-11-05 16:02:23 -0300

    tres

---
+++ 11.Funciones/ParametrosyArgumentos.py
@@ -0,0 +1,17 @@
+def suma():
+    num1 = 20
+    num2 = 30 
+    suma = num1 + num2 
+    return suma 
+
+print(suma())
+
+def suma(num1, num2):
+    suma = num1 + num2
+    return suma
+
+print(suma(10 , 35))
+
+
+
+
---
+++ 11.Funciones/ValoresIndefinidos.py
@@ -0,0 +1,4 @@
+def argumento(*num):# con el asterisco pasa a tupla
+    return type(num)
+
+print(argumento(10))# indica si es un int, float, tupla o cadena.
